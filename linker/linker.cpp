ee56e3c9073c958f59b1654109896fa7b276aa13
Kyle Repinski <repinski23@gmail.com>
2016-08-27 20:19:20 -0500

    linker: Add build flag to allow text relocations for platform libs.
    Change-Id: I2baf6fc092dff1f49ffeecf16faafd7ac35b6491
    
    Conflicts:
    	linker/linker.cpp

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -4013,6 +4013,18 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
 
 #if !defined(__LP64__)
   if (has_text_relocations) {
+#ifndef ALLOW_PLATFORM_TEXTRELS
+    // Fail if app is targeting sdk version > 22
+    if (get_application_target_sdk_version() > 22) {
+#else
+    // Some devices require an exception for platform libs (e.g. vendor libs)
+    if (get_application_target_sdk_version() != __ANDROID_API__ &&
+        get_application_target_sdk_version() > 22) {
+#endif
+      PRINT("%s: has text relocations", get_realpath());
+      DL_ERR("%s: has text relocations", get_realpath());
+      return false;
+    }
     // Make segments writable to allow text relocations to work properly. We will later call
     // phdr_table_protect_segments() after all of them are applied.
     DL_WARN("%s has text relocations. This is wasting memory and prevents "
